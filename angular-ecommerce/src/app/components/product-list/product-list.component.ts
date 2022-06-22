7d414adfa6d085fd8815d35435525b464b889324
yachieh09 <yachiehhuanggg@gmail.com>
2022-06-22 21:31:49 +0800

    Create Shopping Cart component

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -104,4 +104,11 @@ export class ProductListComponent implements OnInit {
     this.listProducts();
   }
 
+  addToCart(theProduct: Product) {
+
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    
+  }
+
 }
