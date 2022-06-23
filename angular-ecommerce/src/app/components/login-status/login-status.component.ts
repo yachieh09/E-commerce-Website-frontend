c3805ad2d1c1f19258a18e5fbbf3c0b28e16c087
yachieh09 <yachiehhuanggg@gmail.com>
2022-06-24 04:35:29 +0800

    Enable login/logout button

--- angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-login-status',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginStatusComponent implements OnInit {
 
-  constructor() { }
+  isAuthenticated: boolean = false;
+  userFullName: string;
+
+  constructor(private oktaAuthService: OktaAuthService) { }
 
   ngOnInit(): void {
+    // Subscribe to authentication state changes
+    this.oktaAuthService.$authenticationState.subscribe(
+      (result) => {
+        this.isAuthenticated = result;
+        this.getUserDetails();
+      }
+    );
   }
 
+  getUserDetails() {
+    if (this.isAuthenticated) {
+
+      // Fetch the logged in user details (user's claims)
+      // user full name is exposed as a property name
+      this.oktaAuthService.getUser().then(
+        (res) => {
+          this.userFullName = res.name;
+        }
+      );
+    }
+  }
+
+  logout() {
+    // Terminates the session with Okta and removes current tokens.
+    this.oktaAuthService.signOut();
+  }
 }
