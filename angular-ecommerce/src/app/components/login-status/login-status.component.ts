911957c2c9d41288c275fc65b64987b2ecdf34b5
yachieh09 <yachiehhuanggg@gmail.com>
2022-06-24 20:03:44 +0800

    Order History: Develop OrderHistory service

--- angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -10,6 +10,7 @@ export class LoginStatusComponent implements OnInit {
 
   isAuthenticated: boolean = false;
   userFullName: string;
+  storage: Storage = sessionStorage;
 
   constructor(private oktaAuthService: OktaAuthService) { }
 
@@ -31,6 +32,12 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuthService.getUser().then(
         (res) => {
           this.userFullName = res.name;
+
+          // retrieve the user's email from authentication response
+          const theEmail = res.email;
+
+          // now store the email in browser storage
+          this.storage.setItem('userEmail', JSON.stringify(theEmail));
         }
       );
     }
--- angular-ecommerce/src/app/config/my-app-config.ts
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc: {
         clientId: '0oa5hgtovj1S8ZQDa5d7',
-        issuer: 'https://dev-98598027.okta.com/ouath2/default',
+        issuer: 'https://dev-98598027.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
