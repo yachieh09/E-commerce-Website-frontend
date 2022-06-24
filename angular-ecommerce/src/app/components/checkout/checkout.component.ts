ca18aaa8f1626245fb8bb3601b1fcbe7cb909fe3
yachieh09 <yachiehhuanggg@gmail.com>
2022-06-25 06:25:25 +0800

    Payment: Add parameters to Spring API

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -236,30 +236,45 @@ export class CheckoutComponent implements OnInit {
 
       this.checkoutService.createPaymentIntent(this.paymentInfo).subscribe(
         (paymentIntentResponse) => {
-           this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
-                                         {payment_method: { card: this.cardElement} }, { handleActions: false })
-                      .then(function (result) {
-                        if (result.error) {
-                            // inform the customer there was an error
-                            alert(`There was an error: ${result.error.message}`);
-                          }
-                        else {
-                            // call REST API via the CheckoutService
-                            this.checkoutService.placeOrder(purchase).subscribe({
-                              next: response => {
-                                alert(`Your order has been received.
+          this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
+            {
+              payment_method: {
+                card: this.cardElement,
+                billing_details: {
+                  email: purchase.customer.email,
+                  name: `${purchase.customer.firstName} ${purchase.customer.lastName}`,
+                  address: {
+                    line1: purchase.billingAddress.street,
+                    city: purchase.billingAddress.city,
+                    state: purchase.billingAddress.state,
+                    postal_code: purchase.billingAddress.zipCode,
+                    country: this.billingAddressCountry.value.code
+                  }
+                }
+              }
+            }, { handleActions: false })
+            .then(function (result) {
+              if (result.error) {
+                // inform the customer there was an error
+                alert(`There was an error: ${result.error.message}`);
+              }
+              else {
+                // call REST API via the CheckoutService
+                this.checkoutService.placeOrder(purchase).subscribe({
+                  next: response => {
+                    alert(`Your order has been received.
                                       \nOrder tracking number: ${response.orderTrackingNumber}`);
 
-                                // reset cart
-                                this.resetCart();
-                              },
-                              error: err => {
-                                alert(`There was an error: ${err.message}`);
-                              }
-                            })
-                          }
-                        }
-                      .bind(this));
+                    // reset cart
+                    this.resetCart();
+                  },
+                  error: err => {
+                    alert(`There was an error: ${err.message}`);
+                  }
+                })
+              }
+            }
+              .bind(this));
         }
       );
     }
@@ -275,6 +290,7 @@ export class CheckoutComponent implements OnInit {
     this.cartService.cartItems = [];
     this.cartService.totalPrice.next(0);
     this.cartService.totalQuantity.next(0);
+    this.cartService.persistCartItems();
 
     // reset the form
     this.checkoutFormGroup.reset();
