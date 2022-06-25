6ae2fbe1cbd2cb21a0f2b4042125c680f2ede964
yachieh09 <yachiehhuanggg@gmail.com>
2022-06-25 15:22:54 +0800

    Payment: Diasable the purchase btn while processing

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -39,6 +39,7 @@ export class CheckoutComponent implements OnInit {
   paymentInfo: PaymentInfo = new PaymentInfo();
   cardElement: any;
   displayError: any = "";
+  isDisabled: boolean = false;
 
   constructor(private formBuilder: FormBuilder,
     private yayShopFormService: YayShopFormService,
@@ -234,6 +235,8 @@ export class CheckoutComponent implements OnInit {
 
     if (!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
 
+      this.isDisabled = true;
+      
       this.checkoutService.createPaymentIntent(this.paymentInfo).subscribe(
         (paymentIntentResponse) => {
           this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
@@ -257,6 +260,7 @@ export class CheckoutComponent implements OnInit {
               if (result.error) {
                 // inform the customer there was an error
                 alert(`There was an error: ${result.error.message}`);
+                this.isDisabled = false;
               }
               else {
                 // call REST API via the CheckoutService
@@ -267,9 +271,11 @@ export class CheckoutComponent implements OnInit {
 
                     // reset cart
                     this.resetCart();
+                    this.isDisabled = false;
                   },
                   error: err => {
                     alert(`There was an error: ${err.message}`);
+                    this.isDisabled = false;
                   }
                 })
               }
